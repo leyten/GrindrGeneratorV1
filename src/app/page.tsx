1740c528d1bd868fb33e838fe34eb78837f6cbb8
leyten <86632281+leyten@users.noreply.github.com>
2024-05-24 12:00:43 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -275,7 +275,7 @@ export default function Home() {
             </div>
             {/* Grid 3  */}
             <div className="">
-              <div className="bg-primary px-4 py-6 rounded-lg xl:h-[560px]">
+              <div className="bg-primary px-4 py-6 rounded-lg xl:h-[460px]">
                 <div className="">
                   <div className="mt-2 grid grid-cols-1 gap-x-6 gap-y-1">
                     <div className="col-span-6">
